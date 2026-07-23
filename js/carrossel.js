85053c063d9ad4c7b9fe090bb00e0903b0d0472f
aLilwall <murigorny@gmail.com>
2026-07-23 09:42:05 -0300

    Add new carrousel images

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,5 +1,16 @@
-function initiateCarrossel() {
-    const images = document.querySelectorAll('.carrossel img');
+function initiateCarrosselMid() {
+    const images = document.querySelectorAll('.carrossel-mid img');
+    let i = 1;
+
+    setInterval(() => {
+        images[i].classList.remove('active');
+        i = (i + 1) % images.length;
+        images[i].classList.add('active');
+    }, 3000)
+}
+
+function initiateCarrosselLeft() {
+    const images = document.querySelectorAll('.carrossel-left img');
     let i = 0;
 
     setInterval(() => {
@@ -9,4 +20,19 @@ function initiateCarrossel() {
     }, 3000)
 }
 
-window.onload = initiateCarrossel;
+function initiateCarrosselRight() {
+    const images = document.querySelectorAll('.carrossel-right img');
+    let i = 2;
+
+    setInterval(() => {
+        images[i].classList.remove('active');
+        i = (i + 1) % images.length;
+        images[i].classList.add('active');
+    }, 3000)
+}
+
+window.onload = () => {
+    initiateCarrosselLeft();
+    initiateCarrosselMid();
+    initiateCarrosselRight();
+};
